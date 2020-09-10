e6755cdf1d6bc4aab13404a32c576bc4b43dde9c
Kukiel <gabrielkukiel@gmail.com>
2020-09-10 19:07:49 -0300

    [NEW] Plannet Component

---
+++ src/components/PlanetComponent/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const PlanetComponent = () => {
+  const planet = "Rodia"
+
+  return (
+    <div>
+      <h2> Você está em {planet}! </h2>
+    </div>
+  )
+
+}
