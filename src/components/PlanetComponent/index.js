95a8dc24521d698ad5d6c4e88fe6b39cd3e44ee3
Kukiel <gabrielkukiel@gmail.com>
2020-09-11 00:13:05 -0300

    [NEW] 2-4

--- src/components/PlanetComponent/index.js
+++ src/components/PlanetComponent/index.js
@@ -2,18 +2,20 @@ import React, {useEffect, useState} from "react"
 import axios from "axios"
 
 export const PlanetComponent = () => {
-  const [planet, setPlanet] = useState("")
+  const [planet, setPlanet] = useState({})
 
   useEffect(() => {
     axios.get('http://swapi.dev/api/planets/1/').then((res) => {
-      setPlanet(res.data.name)
+      setPlanet(res.data)
     })
   }, [])
 
   return (
     <div>
-      <h2> Você está em {planet}! </h2>
+      <h2> Você está em {planet.name}! </h2>
+      <div> População: {planet.population}</div>
+      <div> Duração do dia: {planet.rotation_period}</div>
+      <div> Duração do ano: {planet.orbital_period}</div>
     </div>
   )
-
 }
